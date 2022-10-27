9d917000f2e2d39b2e86487c8def615cf84741de
Thieu Mao <thieumao@gmail.com>
2022-10-28 00:05:45 +0700

    Student model

--- src/main/java/com/language123/quiz/QuizApplication.java
+++ src/main/java/com/language123/quiz/QuizApplication.java
@@ -1,13 +1,31 @@
 package com.language123.quiz;
 
+import com.language123.quiz.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class QuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
+	}
+
+	@GetMapping(value = "/")
+	public List<Student> hello() {
+		return List.of(
+				new Student(1L, "Mao", "thieumao@gmail.com",
+						LocalDate.of(1992, Month.JULY, 13), 30),
+				new Student(2L, "Ha", "thu96ha@gmail.com",
+						LocalDate.of(1996, Month.NOVEMBER, 6), 26)
+		);
 	}
 
 }
