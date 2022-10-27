faf7e4fe6fb2aa7d1356a50bad12ff7a32e31dde
Thieu Mao <thieumao@gmail.com>
2022-10-28 01:11:24 +0700

    Update @Component to @Service

--- src/main/java/com/language123/quiz/student/StudentService.java
+++ src/main/java/com/language123/quiz/student/StudentService.java
@@ -1,12 +1,12 @@
 package com.language123.quiz.student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@Component
+@Service
 public class StudentService {
 
 	public List<Student> getStudents() {
