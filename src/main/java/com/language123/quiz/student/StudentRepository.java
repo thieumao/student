91bb3c11e04423ca792a10c79c8cbc93bcb506e7
Thieu Mao <thieumao@gmail.com>
2022-10-28 21:21:11 +0700

    API: New Student

--- src/main/java/com/language123/quiz/student/StudentRepository.java
+++ src/main/java/com/language123/quiz/student/StudentRepository.java
@@ -1,12 +1,17 @@
 package com.language123.quiz.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+	@Query("SELECT s FROM Student s WHERE s.email = ?1")
+	Optional<Student> findStudentByEmail(String email);
 }
