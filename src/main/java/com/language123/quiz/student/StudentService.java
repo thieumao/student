0f58cbedf1209e0e7d724ead67eab3c42b256040
Thieu Mao <thieumao@gmail.com>
2022-10-28 21:26:34 +0700

    Show message error

--- src/main/java/com/language123/quiz/student/StudentService.java
+++ src/main/java/com/language123/quiz/student/StudentService.java
@@ -25,7 +25,7 @@ public class StudentService {
 	public void addNewStudent(Student student) {
 		Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
 		if (studentOptional.isPresent()) {
-			throw new IllegalStateException("email taken");
+			throw new IllegalStateException("Email taken");
 		}
 		studentRepository.save(student);
 	}
