65b2201e8cd4a27ac391baa9024ccaf8e57568f1
Thieu Mao <thieumao@gmail.com>
2022-10-28 23:59:28 +0700

    Put API

--- src/main/java/com/language123/quiz/student/StudentController.java
+++ src/main/java/com/language123/quiz/student/StudentController.java
@@ -30,4 +30,12 @@ public class StudentController {
 	public void deleteStudent(@PathVariable("studentId") Long studentId) {
 		studentService.deleteStudent(studentId);
 	}
+
+	@PutMapping(value = "/{studentId}")
+	public void updateStudent(
+			@PathVariable("studentId") Long studentId,
+			@RequestParam(required = false) String name,
+			@RequestParam(required = false) String email) {
+		studentService.updateStudent(studentId, name, email);
+	}
 }
--- src/main/java/com/language123/quiz/student/StudentService.java
+++ src/main/java/com/language123/quiz/student/StudentService.java
@@ -1,11 +1,13 @@
 package com.language123.quiz.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -36,5 +38,25 @@ public class StudentService {
 			throw  new IllegalStateException("Student with id " + studentId + " does not exists");
 		}
 		studentRepository.deleteById(studentId);
+	}
+
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email) {
+		Student student = studentRepository.findById(studentId)
+				.orElseThrow(() -> new IllegalStateException(
+						"Student with id " + studentId + " does not exists"
+				));
+		if (name != null && name.length() > 0
+				&& !Objects.equals(student.getName(), name)) {
+			student.setName(name);
+		}
+		if (email != null && email.length() > 0
+				&& !Objects.equals(student.getEmail(), email)) {
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+			if (studentOptional.isPresent()) {
+				throw new IllegalStateException("Email taken");
+			}
+			student.setEmail(email);
+		}
 	}
 }
